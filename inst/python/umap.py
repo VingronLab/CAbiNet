b911b18e71a59fabfb5d07e0bd07b6353bcdf187
ClemensKohl <kohl.clemens@gmail.com>
2023-09-29 16:21:29 +0200

    seed to int

--- inst/python/umap.py
+++ inst/python/umap.py
@@ -1,11 +1,11 @@
 import umap.umap_ as umap
 
 def python_umap(dm, n_neighbors, metric, seed=2358):
-                  
+  
   reducer = umap.UMAP(
         n_neighbors=n_neighbors,
         metric=metric,
-        random_state = seed
+        random_state = int(seed)
         )
         
   embedding = reducer.fit_transform(dm)
